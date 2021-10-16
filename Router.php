deab127b49c8a0361ea4097c555c58d568649b8f
enfrte <5081917+enfrte@users.noreply.github.com>
2021-10-17 02:21:29 +0300

    First working draft

--- Router.php
+++ Router.php
@@ -5,33 +5,60 @@
 class Router {
 
 	private $uri_exploded;
+	private $obj;
 
 	public function __construct($uri)
 	{
-		$this->uri_exploded =  explode('/', $uri);;
+		$uri = strtok($uri, '?'); // Discard query string
+		$uri = rtrim($uri,"/"); 
+		$this->uri_exploded =  explode('/', $uri);
+		$this->obj = new stdClass();
 	}
+
 
 	public function getRoute()
 	{
-		
+		$this->obj->class = $this->getClass();
+		$this->obj->method = $this->getMethod();
+		$this->obj->arguments = $this->getArguments();
+		return $this->obj;
 	}
+
 
 	public function getClass()
 	{
-		return $this->uri_exploded[0];
+		if (!empty($this->uri_exploded[0])) {
+			return $this->uri_exploded[0];
+		}
+
+		return null;
 	}
+
 
 	public function getMethod()
 	{
-		return $this->uri_exploded[1];
+		if (empty($this->uri_exploded[0])) { // No class 
+			return null;
+		}
+		elseif (empty($this->uri_exploded[1])) { // There is a class but no method
+			return 'index'; // default
+		}
+
+		return $this->uri_exploded[1]; // There is a value
 	}
 
+	
 	public function getArguments()
 	{
 		$uri = $this->uri_exploded;
-		// maybe check length
-		$args = [3, ...$uri];
-		return [...$args];
+		$uri_spread = [...$uri];
+
+		if (count($uri_spread) > 2) {
+			$arguments = array_slice($uri_spread, 2); 
+			return $arguments;
+		}
+
+		return null;
 	}
 
 }
--- tests/RouterAssertionsTest.php
+++ tests/RouterAssertionsTest.php
@@ -2,111 +2,113 @@
 
 // php vendor/bin/phpunit tests/RouterAssertionsTest.php
 
-class RouterAssertionsTest extends \PHPUnit\Framework\TestCase {
+use \PHPUnit\Framework\TestCase;
+require 'Router.php';
+
+class RouterAssertionsTest extends TestCase {
 
 	// Get just the class
 	public function testGetClass()
 	{
-		$uris = [
-			'https://www.foo.com/aClass/aMethod/argOne/argTwo',
+		$request_uri = [
+			'aClass/aMethod/argOne/argTwo',
+			'aClass/aMethod/argOne/argTwo/',
+			'aClass/aMethod/argOne/argTwo?foo=123',
 		];
-		$router = new Router($uris[0]);
-		$r = $router->getClass();
 
-		$this->assertTrue($r->class, 'aClass');
-		/*
-		if ($r->class === 'aClass') { 
-			return true;
+		foreach ($request_uri as $uri) {
+			$router = new Router($uri);
+			$class = $router->getClass();
+			$this->assertSame($class, 'aClass');
 		}
-		else {
-			return false;
-		}
-		*/
 	}
-/*
+
+
 	// Get just the method
 	public function testGetMethod()
 	{
-		$uris = [
-			'https://www.foo.com/aClass/aMethod/argOne/argTwo',
+		$request_uri = [
+			'aClass/aMethod/argOne/argTwo',
+			'aClass/aMethod/argOne/argTwo/',
+			'aClass/aMethod/argOne/argTwo?foo=123',
 		];
-		$router = new Router($uris[0]);
-		$r = $router->getMethod();
 
-		if ($r->method === 'aMethod') { 
-			return true;
-		}
-		else {
-			return false;
+		foreach ($request_uri as $uri) {
+			$router = new Router($request_uri[0]);
+			$method = $router->getMethod();
+			$this->assertSame($method, 'aMethod');
 		}
 	}
 
-	// Search with multiple args
-	public function testGetAllArgs()
-	{
-		$uris = [
-			'https://www.foo.com/aClass/aMethod/argOne/argTwo',
-		];
-		$router = new Router($uris[0]);
-		$r = $router->getArguments();
 
-		if ($r->args === ['argOne', 'argTwo']) { 
-			return true;
+	// Search with multiple arguments
+	public function testGetAllArguments()
+	{
+		$request_uri = [
+			'aClass/aMethod/argOne/argTwo',
+			'aClass/aMethod/argOne/argTwo/',
+			'aClass/aMethod/argOne/argTwo?foo=123',
+		];
+		foreach ($request_uri as $uri) {
+			$router = new Router($uri);
+			$arguments = $router->getArguments();
+			$this->assertSame($arguments, ['argOne', 'argTwo']);
 		}
-		else {
-			return false;
-		}	
 	}
 
-	// Search with no args
-	public function testNoArgs()
-	{
-		$uris = [
-			'https://www.foo.com/aClass/aMethod/',
-		];
-		$router = new Router($uris[0]);
-		$r = $router->getRoute();
 
-		if ($r->class === 'aClass' && $r->method === 'aMethod' && empty($r->args)) { 
-			return true;
-		}
-		else {
-			return false;
+	// Search with no arguments
+	public function testNoArguments()
+	{
+		$request_uri = [
+			'aClass/aMethod',
+			'aClass/aMethod/',
+			'aClass/aMethod?foo=123',
+		];
+
+		foreach ($request_uri as $uri) {
+			$router = new Router($uri);
+			$r = $router->getRoute();
+			
+			$this->assertSame($r->class, 'aClass');
+			$this->assertSame($r->method, 'aMethod');
+			$this->assertTrue(empty($r->arguments), true);
 		}
 	} 
+
 
 	// Search with no method - method will be replaced by index
 	public function testNoMethod()
 	{
-		$uris = [
-			'https://www.foo.com/aClass/',
+		$request_uri = [
+			'aClass',
+			'aClass/',
+			'aClass?foo=123',
 		];
-		$router = new Router($uris[0]);
-		$r = $router->getRoute();
 
-		if ($r->class === 'aClass' && $r->method === 'index' && empty($r->args)) { 
-			return true;
-		}
-		else {
-			return false;
+		foreach ($request_uri as $uri) {
+			$router = new Router($uri);
+			$r = $router->getRoute();
+
+			$this->assertSame($r->class, 'aClass');
+			$this->assertSame($r->method, 'index');
+			$this->assertTrue(empty($r->arguments), true);
 		}
 	}
+
 
 	// Search with no route
 	public function testNoRoute()
 	{
-		$uris = [
-			'https://www.foo.com/',
+		$request_uri = [
+			'',
 		];
-		$router = new Router($uris[0]);
+		$router = new Router($request_uri[0]);
 		$r = $router->getRoute();
 
-		if (empty($r->class) && empty($r->method) && empty($r->args)) { 
-			return true;
-		}
-		else {
-			return false;
-		}	
+		$this->assertTrue(empty($r->class), true);
+		$this->assertTrue(empty($r->method), true);
+		$this->assertTrue(empty($r->arguments), true);
 	}
-*/
+
 }
